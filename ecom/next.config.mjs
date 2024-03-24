b46197f4d20579ab676597669f620af577a29800
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-25 00:24:35 +0530

    project 3

--- ecom/next.config.mjs
+++ ecom/next.config.mjs
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "./base.scss";`
+  }
 };
 
 export default nextConfig;
