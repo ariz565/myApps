190f0765f225236bd13d2fac941dfd19189e5d08
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-26 05:45:47 +0530

    p4

---
+++ myappui/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+        // You don't need port or pathname if you want to allow everything under the domain
+      },
+    ],
+  },
+};
